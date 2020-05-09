75da0ee6742e52bc862674552880173f1af99015
Nkemakolam Nnadi <nkemakolamnnadi@Nkemakolams-MBP.fios-router.home>
2020-05-09 15:53:09 -0400

    using a fucntion to get user input

--- blockchain/blockchain.py
+++ blockchain/blockchain.py
@@ -1,17 +1,21 @@
-blockchain =[]
+blockchain = []
+
 
 def get_last_blockchain_value():
     return blockchain[-1]
 
 
-def add_value(transaction_amount,last_transaction=[1]):
-    blockchain.append([last_transaction ,transaction_amount])
+def add_value(transaction_amount, last_transaction=[1]):
+    blockchain.append([last_transaction, transaction_amount])
     print(blockchain)
 
-tx_amount=float(input('Your tarnasaction amount please: '))
+def get_user_input():
+    return float(input("Your tarnasaction amount please: "))
+
+tx_amount = get_user_input()
 
 add_value(tx_amount)
-tx_amount=float(input('Your tarnasaction amount please: '))
-add_value(last_transaction=get_last_blockchain_value(),transaction_amount=tx_amount3)
-tx_amount=float(input('Your tarnasaction amount please: '))
-add_value(tx_amount,get_last_blockchain_value())
+tx_amount = get_user_input()
+add_value(last_transaction=get_last_blockchain_value(), transaction_amount=tx_amount)
+tx_amount = get_user_input()
+add_value(tx_amount, get_last_blockchain_value())
