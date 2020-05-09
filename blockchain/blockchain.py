10d8dd7fe65f694a07f4cae13cf7e45b0bb16f3b
Nkemakolam Nnadi <nkemakolamnnadi@Nkemakolams-MBP.fios-router.home>
2020-05-09 14:27:55 -0400

    spliting function

--- blockchain/blockchain.py
+++ blockchain/blockchain.py
@@ -1,10 +1,13 @@
 blockchain =[[1]]
 
+def get_last_blockchain_value():
+    return blockchain[-1]
+
 
 def add_value(transaction_amount):
-    blockchain.append([blockchain[-1] ,transaction_amount])
+    blockchain.append([get_last_blockchain_value() ,transaction_amount])
     print(blockchain)
 
-add_value(2.4)
+add_value(2)
 add_value(2.5)
-add_value(1.5)
+add_value(3.5)
