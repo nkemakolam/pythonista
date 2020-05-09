f6d7df0d4057441b15d0381a7b255c80dd2cc984
Nkemakolam Nnadi <nkemakolamnnadi@Nkemakolams-MBP.fios-router.home>
2020-05-09 15:32:56 -0400

    adding the standard read input built in function

--- blockchain/blockchain.py
+++ blockchain/blockchain.py
@@ -8,6 +8,8 @@ def add_value(transaction_amount,last_transaction=[1]):
     blockchain.append([last_transaction ,transaction_amount])
     print(blockchain)
 
-add_value(2)
+tx_amount=float(input('Your tarnasaction amount please: '))
+
+add_value(tx_amount)
 add_value(last_transaction=get_last_blockchain_value(),transaction_amount=2.5)
 add_value(3.5,get_last_blockchain_value())
