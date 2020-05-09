3efc1f864b7c66654a4ecd3dcf6091ee61de3536
Nkemakolam Nnadi <nkemakolamnnadi@Nkemakolams-MBP.fios-router.home>
2020-05-09 15:34:57 -0400

    passing the input as a transaction break in the function call

--- blockchain/blockchain.py
+++ blockchain/blockchain.py
@@ -11,5 +11,7 @@ def add_value(transaction_amount,last_transaction=[1]):
 tx_amount=float(input('Your tarnasaction amount please: '))
 
 add_value(tx_amount)
-add_value(last_transaction=get_last_blockchain_value(),transaction_amount=2.5)
-add_value(3.5,get_last_blockchain_value())
+tx_amount=float(input('Your tarnasaction amount please: '))
+add_value(last_transaction=get_last_blockchain_value(),transaction_amount=tx_amount3)
+tx_amount=float(input('Your tarnasaction amount please: '))
+add_value(tx_amount,get_last_blockchain_value())
