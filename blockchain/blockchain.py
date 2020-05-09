6e7b9fa2e523ea1102588d294cc3e560cb1263ec
Nkemakolam Nnadi <nkemakolamnnadi@Nkemakolams-MBP.fios-router.home>
2020-05-09 14:36:22 -0400

    using default value in function

--- blockchain/blockchain.py
+++ blockchain/blockchain.py
@@ -4,10 +4,10 @@ def get_last_blockchain_value():
     return blockchain[-1]
 
 
-def add_value(transaction_amount,last_transaction):
+def add_value(transaction_amount,last_transaction=[1]):
     blockchain.append([last_transaction ,transaction_amount])
     print(blockchain)
 
-add_value(2,[1])
+add_value(2)
 add_value(2.5,get_last_blockchain_value())
 add_value(3.5,get_last_blockchain_value())
