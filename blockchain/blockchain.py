2c9d14e3ebdd0f98a755148e1b4edf65c637c43f
Nkemakolam Nnadi <nkemakolamnnadi@Nkemakolams-MBP.fios-router.home>
2020-05-09 17:48:38 -0400

    adding more loop

--- blockchain/blockchain.py
+++ blockchain/blockchain.py
@@ -17,14 +17,19 @@ def get_user_input():
     return float(input("Your tarnasaction amount please: "))
 
 tx_amount = get_user_input()
-
 add_value(tx_amount)
-tx_amount = get_user_input()
-add_value(last_transaction=get_last_blockchain_value(), transaction_amount=tx_amount)
-tx_amount = get_user_input()
-add_value(tx_amount, get_last_blockchain_value())
+
+""" Here we could stop usin the repition of user input using while loop this could make a very good 
+   interface  as would be demonstrated below
+   note that while true would always execute becaus etrue will always be true
+"""
+while True:
+    tx_amount = get_user_input()
+    add_value(tx_amount, get_last_blockchain_value())
 
 # Looping through the block here we would be using the two type of loo in  python to loop through the block chain
-for block in blockchain:
-    print('Outputting Block')
-    print(block)
+    for block in blockchain:
+        print('Outputting Block')
+        print(block)
+        
+Print("Done!")
--- blockchain/loops.py
+++ blockchain/loops.py
@@ -3,5 +3,15 @@ the arr the for loop and the while loop note that the for loop is used to
 loop over an iterable like a list 
  the for loop goes ove the iterable numbers of ti e that is equalent to the lenght of the iterable
  while loop on the other hand execute on tile a certain condition
- is true
+ is true see sample of the for and the while loop
 """
+while True:
+    tx_amount = get_user_input()
+    add_value(tx_amount, get_last_blockchain_value())
+
+# Looping through the block here we would be using the two type of loo in  python to loop through the block chain
+    for block in blockchain:
+        print('Outputting Block')
+        print(block)
+
+"""" Here we want to talk about conditional in python 
