559613fcd52cef08ec731ae2d169903c73651994
Nkemakolam Nnadi <nkemakolamnnadi@Nkemakolams-MBP.fios-router.home>
2020-05-09 14:35:22 -0400

    re arranging the function

--- blockchain/blockchain.py
+++ blockchain/blockchain.py
@@ -1,13 +1,13 @@
-blockchain =[[1]]
+blockchain =[]
 
 def get_last_blockchain_value():
     return blockchain[-1]
 
 
-def add_value(transaction_amount):
-    blockchain.append([get_last_blockchain_value() ,transaction_amount])
+def add_value(transaction_amount,last_transaction):
+    blockchain.append([last_transaction ,transaction_amount])
     print(blockchain)
 
-add_value(2)
-add_value(2.5)
-add_value(3.5)
+add_value(2,[1])
+add_value(2.5,get_last_blockchain_value())
+add_value(3.5,get_last_blockchain_value())
