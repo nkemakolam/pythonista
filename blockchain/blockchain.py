64db67637f97d7693de9255d998706aec383c679
Nkemakolam Nnadi <nkemakolamnnadi@Nkemakolams-MBP.fios-router.home>
2020-05-09 15:28:23 -0400

    ding default value and calling it

--- blockchain/blockchain.py
+++ blockchain/blockchain.py
@@ -9,5 +9,5 @@ def add_value(transaction_amount,last_transaction=[1]):
     print(blockchain)
 
 add_value(2)
-add_value(2.5,get_last_blockchain_value())
+add_value(last_transaction=get_last_blockchain_value(),transaction_amount=2.5)
 add_value(3.5,get_last_blockchain_value())
