d1c6718bf25f8451c2295b6067e0da1f4a53de33
Nkemakolam Nnadi <nkemakolamnnadi@Nkemakolams-MBP.fios-router.home>
2020-05-09 17:27:32 -0400

    adding loop to the block chain

--- blockchain/blockchain.py
+++ blockchain/blockchain.py
@@ -23,3 +23,8 @@ tx_amount = get_user_input()
 add_value(last_transaction=get_last_blockchain_value(), transaction_amount=tx_amount)
 tx_amount = get_user_input()
 add_value(tx_amount, get_last_blockchain_value())
+
+# Looping through the block here we would be using the two type of loo in  python to loop through the block chain
+for block in blockchain:
+    print('Outputting Block')
+    print(block)
---
+++ blockchain/loops.py
@@ -0,0 +1,7 @@
+""" There are two type of loops in python 
+the arr the for loop and the while loop note that the for loop is used to 
+loop over an iterable like a list 
+ the for loop goes ove the iterable numbers of ti e that is equalent to the lenght of the iterable
+ while loop on the other hand execute on tile a certain condition
+ is true
+"""
