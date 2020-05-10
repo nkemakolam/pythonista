3ab36e6700426f6ac0bb73689cf454235ded19fc
Nkemakolam Nnadi <nkemakolamnnadi@Nkemakolams-MBP.fios-router.home>
2020-05-10 00:48:50 -0400

    add the is and the in built in function

---
+++ blockchain/booleandemo.py
@@ -0,0 +1,17 @@
+""" python has the is and in boolean operator that you 
+can use for some of the function  it does to compare and return true or
+false transaction the folloeing example would show
+""" 
+
+print(1==1)
+name = 'nkem'
+print(name != 'chris')
+print(1 is 1)
+data = [1,2,3]
+data_test=[1,2,3]
+print(data == data_test)
+print(data is data_test)
+print( 1 in data)
+print( 5 in data_test)
+print( 5 nit in data)
+print(data is not data_test)
