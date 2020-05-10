db169d3d0f61594e1015859e60a92334a7e208b7
Nkemakolam Nnadi <nkemakolamnnadi@Nkemakolams-MBP.fios-router.home>
2020-05-10 00:36:20 -0400

    adding more logic to the last_transaction

--- blockchain/blockchain.py
+++ blockchain/blockchain.py
@@ -2,6 +2,8 @@ blockchain = []
 
 """ Returun the last value of the current blockchain"""
 def get_last_blockchain_value():
+    if len(blockchain) < 1:
+        return None
     return blockchain[-1]
 
 """ Append a new value to and as well as last blockchain value to the blockchain 
@@ -9,7 +11,9 @@ Arguments:
        : transaction_amount : the amount that should be added
        :last_transaction: the last blockchain transaction(default [1])
 """
-def add_value(transaction_amount, last_transaction=[1]):
+def add_transaction(transaction_amount, last_transaction=[1]):
+    if last_transaction == None:
+        last_transaction = [1]
     blockchain.append([last_transaction, transaction_amount])
     print(blockchain)
 
@@ -27,9 +31,6 @@ def print_blockchain_element():
         print('Outputting Block')
         print(block)
 
-tx_amount = get_transaction_value()
-add_value(tx_amount)
-
 """ Here we could stop usin the repition of user input using while loop this could make a very good 
    interface  as would be demonstrated below
    note that while true would always execute becaus etrue will always be true
@@ -42,7 +43,7 @@ while True:
     user_choice = get_user_choice()
     if user_choice == '1':
         tx_amount = get_transaction_value()
-        add_value(tx_amount, get_last_blockchain_value())
+        add_transaction(tx_amount, get_last_blockchain_value())
     elif user_choice == '2':
         print_blockchain_element()
     elif user_choice == 'q':
