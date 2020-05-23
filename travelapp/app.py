10a40ceab5c63c3a010c2c7fdccbf8ff860d6161
Nkemakolam Nnadi <nkemakolamnnadi@Nkemakolams-MBP.fios-router.home>
2020-05-22 23:26:37 -0400

    using conditional flow

--- travelapp/app.py
+++ travelapp/app.py
@@ -5,13 +5,12 @@ app = Flask(__name__)
 #Defining the root of the app
 @app.route("/")
 def index():
-    return "<h1> hello world</h1>"
+    return render_template("static/templates/master.html", message="this is the home")
 #Defining the about Page
 @app.route("/about")
 def about():
-    return "<h1> hello world about</h1>"
-
+    return render_template("static/templates/master.html", message="this is the about")
 # returning the Contact us page
 @app.route("/contact")
 def contact():
-    return "<h1> hello world Contact</h1>"
+     return render_template("static/templates/master.html", message="this is the contact")
